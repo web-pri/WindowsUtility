624fa6807c9de7791ead312387c7d88ec033e7dc
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-09 09:39:43 +0800

    Create DelTemp.bat

---
+++ DelTemp.bat
@@ -0,0 +1,9 @@
+@echo off
+title Temp Deleter
+echo This .bat file will delete temp files, and may make your device faster.
+pause
+del %temp%
+cls
+echo Temp files deleted. you may now close this window.
+pause
+exit
